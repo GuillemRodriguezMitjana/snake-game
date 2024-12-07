2cf870643569a26c2327b67550598b9b8d2dd308
Guillem Rodríguez Mitjana <guillem.rodriguez@estudiants.urv.cat>
2024-12-07 19:49:11 +0100

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.js
+
+import GameBoard from './components/GameBoard';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Snake Game</h1>
+            <GameBoard />
+        </div>
+    );
+}
+
+export default App;
