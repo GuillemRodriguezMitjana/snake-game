a478c7270f4e84d214743edf1d548b4f5586b3b9
Guillem Rodríguez Mitjana <guillem.rodriguez@estudiants.urv.cat>
2024-12-09 09:15:05 +0100

    Domini de la API posat al .env

--- snake-game-web/src/api.js
+++ snake-game-web/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api/v1/player",
+  baseURL: `http://${import.meta.env.VITE_API_DOMAIN}/api/v1/player`,
   headers: {
     "Content-Type": "application/json",
   },
