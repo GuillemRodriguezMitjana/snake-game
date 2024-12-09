a665ffaec3de08f78a6e371b44f2b7472bc94ee6
Guillem Rodríguez Mitjana <guillem.rodriguez@estudiants.urv.cat>
2024-12-09 09:27:14 +0100

    Afegit el protocol web (http/https) a la variable d'entorn amb la url de la api

--- snake-game-web/src/api.js
+++ snake-game-web/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `http://${import.meta.env.VITE_API_DOMAIN}/api/v1/player`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1/player`,
   headers: {
     "Content-Type": "application/json",
   },
